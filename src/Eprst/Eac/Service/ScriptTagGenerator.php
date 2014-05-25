322910bcd008b24b0de17854a704c2c49f3f671c
Artur Safin <treilor@gmail.com>
2014-05-25 22:23:18 +0300

    Add ScriptTagGenerator implementation

--- src/Eprst/Eac/Service/ScriptTagGenerator.php
+++ src/Eprst/Eac/Service/ScriptTagGenerator.php
@@ -1,8 +1,6 @@
 <?php
 
-
 namespace Eprst\Eac\Service;
-
 
 class ScriptTagGenerator
 {
@@ -15,6 +13,16 @@ class ScriptTagGenerator
      */
     public function generate($compileFile, $webroot, $prefix)
     {
+        $compileFile = realpath($compileFile);
+        $webroot = realpath($webroot);
 
+        $compileFile = mb_substr(realpath($compileFile), mb_strlen($webroot));
+
+        return $this->getTag($prefix . '/' . ltrim($compileFile, "/\\"));
+    }
+
+    private function getTag($src)
+    {
+        return sprintf("<script type='text/javascript' src='%s'></script>", $src);
     }
 } 
